bd7499f807c114fae15de64c3a838693d4139e06
LarSantaTerezinha <projetolarsantaterezinha@gmail.com>
2023-09-15 08:09:29 -0300

    teste

--- LarSanta-Terezinha/valida_update_usuario.php
+++ LarSanta-Terezinha/valida_update_usuario.php
@@ -23,7 +23,7 @@ mysqli_close($con);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Atualiza Vinho</title>
+    <title>Atualização Usuário</title>
 </head>
 <link href="css/bootstrap.min.css" rel="stylesheet">
 <body>
